2fd7f4ead630967a0620d2410095c1d48ccaca82
Ludovic Courtes <ludovic.courtes@laas.fr>
2007-05-18 09:31:48 +0000

    Added a basic test framework.
    * Makefile.am (SUBDIRS): Added `tests'.
    
    * configure.ac: Removed `gl_EARLY' invocation.
      Produce `tests/Makefile'.
    
    * src/Makefile.am (noinst_HEADERS): Added "utils.h".
      (AM_CPPFLAGS): Added `-I$(srcdir)'.
    
    git-archimport-id: lcourtes@laas.fr--2006-libre/guile-avahi--devo--0--patch-4

---
+++ tests/simple-poll.scm
@@ -0,0 +1,62 @@
+;;; Guile-Avahi --- Guile bindings for Avahi.
+;;; Copyright (C) 2007  Ludovic Courtès <ludovic.courtes@laas.fr>
+;;;
+;;; Guile-Avahi is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU Lesser General Public
+;;; License as published by the Free Software Foundation; either
+;;; version 2.1 of the License, or (at your option) any later version.
+;;;
+;;; Guile-Avahi is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with Guile-Avahi; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+;;;
+;;; Excercise the simple-poll and client APIs, assuming the Avahi daemon is
+;;; reachable.
+;;;
+
+(use-modules (avahi)
+             (avahi client)
+             (srfi srfi-1))
+
+(dynamic-wind
+    (lambda ()
+      #t)
+
+    (lambda ()
+      (define (client-callback client state)
+        (if (not (and (client? client)
+                      (string? (client-state->string state))))
+            (throw 'failure)))
+
+      (exit (and (simple-poll? (make-simple-poll))
+                 (every poll?
+                        (let loop ((polls '())
+                                   (count 123))
+                          (if (= count 0)
+                              polls
+                              (begin
+                                (gc)
+                                (loop (cons (simple-poll (make-simple-poll))
+                                            polls)
+                                      (- count 1))))))
+                 (let* ((poll (make-simple-poll))
+                        (client (make-client (simple-poll poll)
+                                             (list
+                                              client-flag/ignore-user-config)
+                                             client-callback)))
+                   (client? client)
+                   (iterate-simple-poll poll 700)
+                   #t))))
+
+    (lambda ()
+      ;; failure.
+      (exit 1)))
+
+
+;;; arch-tag: b866efb9-235f-4005-b9ca-8b246e0c8f80
